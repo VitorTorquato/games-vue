69e2a2d6314049517c3bc24a12f6632dd74a58bb
Vitor Torquato de Sousa <torquatovitor68@gmail.com>
2025-03-04 15:10:37 +0100

    GameCards created

---
+++ src/components/GameCard/GameCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import { defineProps } from 'vue';
+  import type { GameProps } from '@/utils/types/game.type';
+
+  defineProps<{
+    data:GameProps
+  }>()
+
+</script>
+
+<template>
+    <section class="w-full bg-slate-200 rounded-lg p-4 mb-5 overflow-hidden">
+      <div class="w-full h-56 hover:scale-105 transition-all duration-300">
+        <img class="object-cover w-full h-full rounded-lg" :src="data.image_url" :alt="data.title">
+      </div>
+      <div class="flex items-center justify-between mt-4">
+        <div class="text-sm font-bold px-2 text-black text-ellipsis truncate whitespace-nowrap overflow-hidden">
+          <p>{{ data.title }}</p>
+        </div>
+      </div>
+    </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,37 @@
 <script setup lang="ts">
 
+import GameCard from '@/components/GameCard/GameCard.vue';
 import HeroSection from '@/components/Hero/HeroSection.vue';
 import type { GameProps } from '@/utils/types/game.type';
 import { onMounted, ref } from 'vue';
 
-    const game = ref<GameProps | null>(null)
+    const gameDaily = ref<GameProps | null>(null);
+    const games = ref<GameProps[]>([]);
 
     const getGamesDaily = async () => {
         try{
           const response = await fetch(`https://sujeitoprogramador.com/next-api/?api=game_day`)
           const data = await response.json();
-          game.value = data
+          gameDaily.value = data
         }catch(error){
           console.error("Something went wrong to fetch data" , error)
+        }
+      }
+
+      const getGameDay = async () => {
+        try{
+          const response = await fetch(`https://sujeitoprogramador.com/next-api/?api=games`)
+          const data =  await response.json();
+          games.value = data
+        }catch(error){
+          console.error("Error to fetch data" , error)
         }
       }
 
 
     onMounted(async () => {
         getGamesDaily();
+        getGameDay();
     })
 
 
@@ -27,7 +40,16 @@ import { onMounted, ref } from 'vue';
 
 <template>
   <main class="w-full bg-black">
-    <HeroSection :link="game?.title" :src="game?.image_url" :alt="game?.title"/>
+    <HeroSection :link="gameDaily?.title" :src="gameDaily?.image_url" :alt="gameDaily?.title"/>
+
+    <div class="w-full bg-slate-900 py-12">
+
+      <h2 class="text-slate-200 text-3xl text-center mb-11">Jogos para conhecer</h2>
+
+      <section class="container mx-auto grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-3">
+          <GameCard v-for="game in games" :key="game.id" :data="game"/>
+      </section>
+    </div>
   </main>
 </template>
 
